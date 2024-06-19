f229ccfa9782e38542c70f84746adc01e2237cf0
Sanket Naik <sanketnaik99@gmail.com>
2024-06-18 22:42:16 -0500

    Fix build errors

--- db/actions/stats.ts
+++ db/actions/stats.ts
@@ -1,20 +1,21 @@
 'use server';
 
 import { revalidatePath } from 'next/cache';
-import { supabase } from '..';
+import { supabaseServer } from '..';
 
 export const getStats = async () => {
-    return await supabase.from( 'stats' ).select( '*' );
+    const { data: stats } = await supabaseServer.from( 'stats' ).select( '*' );
+    return stats ?? [];
 };
 
 export const addStat = async ( name: string ) => {
-    const result = await supabase.from( 'stats' ).insert( { name } );
+    const result = await supabaseServer.from( 'stats' ).insert( { name } );
     revalidatePath( '/admin/stats' );
     return result;
 };
 
 export const deleteStat = async ( id: number ) => {
-    const result = await supabase.from( 'stats' ).delete().eq( 'id', id );
+    const result = await supabaseServer.from( 'stats' ).delete().eq( 'id', id );
     revalidatePath( '/admin/stats' );
     return result;
 };
--- db/index.ts
+++ db/index.ts
@@ -1,3 +1,9 @@
+import { createBrowserClient } from '@supabase/ssr';
 import { createClient } from '@/utils/supabase/client';
 
 export const supabase = createClient();
+
+export const supabaseServer = createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_KEY!
+);
